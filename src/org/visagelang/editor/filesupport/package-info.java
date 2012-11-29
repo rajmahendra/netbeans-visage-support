86c9ca5495cda5061a57b291efc833931a0575eb
rajmahendra <rajmahendra@gmail.com>
2012-11-29 18:02:46 +0530

    Initial commit

---
+++ src/org/visagelang/editor/filesupport/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@TemplateRegistration(folder = "Other", content = "VisageTemplate.visage")
+package org.visagelang.editor.filesupport;
+
+import org.netbeans.api.templates.TemplateRegistration;
